5ac7206c98b826ad3f03eb4a50b28d29ac04362b
Grena30 <grenadinu@yahoo.com>
2022-12-23 16:35:44 +0200

    tables

---
+++ server/model/tables.js
@@ -0,0 +1,52 @@
+const Sequelize = require("sequelize");
+const sequelize = require('./sq').sequelize
+
+const country = sequelize.define("country", {
+    country_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      required: true,
+      allowNull: false
+    },
+    status_id: {
+      type: Sequelize.STRING,
+      required: true,
+      allowNull: false
+    },
+ });
+
+const status = sequelize.define("country", {
+    status_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    description: {
+      type: Sequelize.STRING,
+      required: true,
+      allowNull: false
+    },
+ });
+ 
+
+country.hasOne(status, {
+    foreignKey: {
+        name: "status_id"
+    }
+})
+
+
+
+
+
+const createTable = sequelize.sync().then(() => {
+    console.log('Tables were created successfully!');
+ }).catch((error) => {
+    console.error('Unable to create the tables : ', error);
+ }); 
+
+ module.exports = {country, status};
