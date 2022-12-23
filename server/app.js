c68e1449e939f95b805d21d09cdaf5b4cb02a10e
Grena30 <grenadinu@yahoo.com>
2022-12-23 13:53:01 +0200

    mysql con

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express();
 const port = 5000;
+const mysql = require('mysql')
 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "password",
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+});
 
 
 app.listen(port, () => {
